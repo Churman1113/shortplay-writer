54e9ba7e744d405be79e00365399077aed4044fd
Churman1113 <churman1113@outlook.com>
2026-04-02 17:52:43 +0800

    Add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
