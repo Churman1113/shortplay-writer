ce3a602515e8c2b86eda7c348dde64bcc3771b6c
Churman1113 <churman1113@outlook.com>
2026-04-02 17:55:23 +0800

    Add src/data/index.ts

---
+++ src/data/index.ts
@@ -0,0 +1,8 @@
+/**
+ * 数据文件统一导出
+ */
+
+export { INDUSTRY_DATA, GUEST_STATS, MOCK_RECENT_SCRIPTS, GENRE_TRENDS, AI_TIPS } from '@/lib/constants'
+export { industryReportData } from './industryReportData'
+export { platforms, mockScript } from './submissionData'
+export type { Platform } from './submissionData'
